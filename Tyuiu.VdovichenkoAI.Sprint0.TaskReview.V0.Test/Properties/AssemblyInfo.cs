c8c8097c43b6e6f9c0a8b9a7acf2a8cbd6014e00
Tyuiu.VdovichenkoAI@outlook.com <a.vdovichenko10@gmail.com>
2023-09-19 13:05:29 +0500

    Add project files.

---
+++ Tyuiu.VdovichenkoAI.Sprint0.TaskReview.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VdovichenkoAI.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VdovichenkoAI.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d752ecfa-3464-4265-9034-b5e045553f4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VdovichenkoAI.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.VdovichenkoAI.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.VdovichenkoAI.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            
+            Console.WriteLine("(x + y + z)*5");
+
+            Console.WriteLine(DataService.Calc(x, y, z));
+
+            Console.ReadKey();
+        }
+    }
+}
